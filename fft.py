4a90a53b1a5f2e34287ed277c5cac7ae547631df
Colin Drewes <colindrewes@gmail.com>
2022-04-15 23:12:40 -0700

    Finalize configs

--- fft.py
+++ fft.py
@@ -8,7 +8,7 @@ import multiprocessing
 import random
 from scipy.fft import fft
 
-def multi_proc_fft(app, seg_len, ffts_per_trace, detrend, half_trace, data_dir_train, data_dir_test, data_dir):
+def multi_proc_fft(app, seg_len, ffts_per_trace, detrend, half_trace, data_dir_train, data_dir_test, data_dir, transition):
   print(app)
   count = 0 
   num_traces = 100
@@ -52,7 +52,10 @@ def multi_proc_fft(app, seg_len, ffts_per_trace, detrend, half_trace, data_dir_t
       # Encode pos and neg edges in two dimensions
       # fft_res = np.stack((neg_freq, pos_freq), axis=-1) 
       # fft_res = neg_freq
-      fft_res = pos_freq
+      if transition == 'pos':
+          fft_res = pos_freq
+      elif transition == 'neg':
+          fft_res = neg_freq
       # fft_res = np.concatenate((neg_freq, pos_freq), axis=None) # For half trace
 
       if count < num_train:
@@ -62,11 +65,11 @@ def multi_proc_fft(app, seg_len, ffts_per_trace, detrend, half_trace, data_dir_t
       count+=1
 
 
-def generate_FFT(data_sets, seg_len, ffts_per_trace, detrend, half_trace, train_dir, test_dir):
+def generate_FFT(data_sets, seg_len, ffts_per_trace, detrend, half_trace, train_dir, test_dir, transition):
     starttime = time.time()
     processes = []
     for run in data_sets: 
-      p = multiprocessing.Process(target=multi_proc_fft, args=(run, seg_len, ffts_per_trace, detrend, half_trace, train_dir, test_dir, data_sets[run]))
+      p = multiprocessing.Process(target=multi_proc_fft, args=(run, seg_len, ffts_per_trace, detrend, half_trace, train_dir, test_dir, data_sets[run], transition))
       processes.append(p)
       p.start()
 
@@ -124,11 +127,46 @@ if __name__ == "__main__":
   # ]
 
   # Pos edge MAX phi and MAX theta with bg sub
+  #boards = [
+  #  'd1d73bec-126f-11ec-bffa-00183e02912e',
+  #  'e5fb3dda-126f-11ec-af7d-00056b00f8ca',
+  #  'f0d751da-126f-11ec-bb74-00056b00b6c5',
+  #  'fa510490-126f-11ec-ad4b-00056b0100fd',
+  #  'df9b8668-153d-11ec-bb14-00183e0248fe'
+  #]
+
+  # ALL
   boards = [
-    'd1d73bec-126f-11ec-bffa-00183e02912e',
-    'e5fb3dda-126f-11ec-af7d-00056b00f8ca',
-    'f0d751da-126f-11ec-bb74-00056b00b6c5',
-    'fa510490-126f-11ec-ad4b-00056b0100fd'
+          'neg_edge_max_phi_max_theta/60578572-0904-11ec-b64d-00056b00b6c5',
+          'neg_edge_max_phi_max_theta/90e5659c-0904-11ec-a4ba-00183e0248fe',
+          'neg_edge_max_phi_max_theta/6f1e8076-094d-11ec-a2d2-00056b00f8ca',
+          'neg_edge_max_phi_max_theta/74ef25f4-094e-11ec-bcb9-00056b0100fd',
+          'neg_edge_max_phi_max_theta/ad63c212-0973-11ec-ab31-00183e02912e',
+          'neg_edge_max_phi_max_theta_bg_sub/03bcc8ae-0ae9-11ec-a87e-00183e0248fe',
+          'neg_edge_max_phi_max_theta_bg_sub/269427fa-0ae9-11ec-8607-00183e02912e',
+          'neg_edge_max_phi_max_theta_bg_sub/15184b50-0ae9-11ec-896c-00056b00f8ca',
+          'neg_edge_max_phi_max_theta_bg_sub/103e5f2a-0ae9-11ec-ae28-00056b00b6c5',
+          'neg_edge_max_phi_max_theta_bg_sub/1912b3e4-0ae9-11ec-b551-00056b0100fd',
+          'neg_edge_min_phi_max_theta_bg_sub/b3ed5b48-0d4c-11ec-b4de-00183e0248fe',
+          'neg_edge_min_phi_max_theta_bg_sub/8e84acd0-0d4c-11ec-b88d-00183e02912e',
+          'neg_edge_min_phi_max_theta_bg_sub/7d491686-0d4c-11ec-b799-00056b00b6c5',
+          'neg_edge_min_phi_max_theta_bg_sub/791be624-0d4c-11ec-ad11-00056b00f8ca',
+          'neg_edge_min_phi_max_theta_bg_sub/6e408fca-0d4c-11ec-843c-00056b0100fd',
+          'pos_edge_min_phi_min_theta/218e2bce-0f95-11ec-9973-00056b00f8ca',
+          'pos_edge_min_phi_min_theta/39aa0ba6-0f95-11ec-99b8-00056b00b6c5',
+          'pos_edge_min_phi_min_theta/43cb4000-0f95-11ec-bc1d-00183e02912e',
+          'pos_edge_min_phi_min_theta/4c1b5ac4-0f95-11ec-987a-00183e0248fe',
+          'pos_edge_min_phi_min_theta/8c43f620-0f94-11ec-ab10-00056b0100fd',
+          'neg_edge_min_phi_max_theta/1ab5997c-10ee-11ec-a440-00183e0248fe',
+          'neg_edge_min_phi_max_theta/6037d892-10ef-11ec-b6bc-00183e02912e',
+          'neg_edge_min_phi_max_theta/6395d75a-10ef-11ec-a0ef-00056b00b6c5',
+          'neg_edge_min_phi_max_theta/81c0fa5c-10ef-11ec-ae62-00056b00f8ca',
+          'neg_edge_min_phi_max_theta/91ae1314-10ef-11ec-8ad0-00056b0100fd',
+          'pos_edge_max_phi_max_theta_bg_sub/d1d73bec-126f-11ec-bffa-00183e02912e',
+          'pos_edge_max_phi_max_theta_bg_sub/e5fb3dda-126f-11ec-af7d-00056b00f8ca',
+          'pos_edge_max_phi_max_theta_bg_sub/f0d751da-126f-11ec-bb74-00056b00b6c5',
+          'pos_edge_max_phi_max_theta_bg_sub/fa510490-126f-11ec-ad4b-00056b0100fd',
+          'pos_edge_max_phi_max_theta_bg_sub/df9b8668-153d-11ec-bb14-00183e0248fe'
   ]
 
   for board in boards:
@@ -149,8 +187,8 @@ if __name__ == "__main__":
         'cortex-present': f'./power_traces/{board}/cortex_present'
       }
 
-      data_dir_train = f"./segments/fft/{board}/Train_Segments_Pos_Detrend_8K/"
-      data_dir_test = f"./segments/fft/{board}/Test_Segments_Pos_Detrend_8K/"
+      data_dir_train = f"./segments/fft/{board}/Train_Segments_Detrend_8K/"
+      data_dir_test = f"./segments/fft/{board}/Test_Segments_Detrend_8K/"
 
       # Create directories if they don't already exist
       if not os.path.exists(data_dir_train):
@@ -163,4 +201,7 @@ if __name__ == "__main__":
       ffts_per_trace = 10
       detrend = True
 
-      generate_FFT(data_set, seg_len, ffts_per_trace, detrend, half_trace, data_dir_train, data_dir_test)
+      transition = board.split('_')[0]
+      print(transition)
+
+      generate_FFT(data_set, seg_len, ffts_per_trace, detrend, half_trace, data_dir_train, data_dir_test, transition)
